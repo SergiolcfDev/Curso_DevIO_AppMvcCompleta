deb51187fa9bec6a619465ca7b30318c0614f7f3
Sergio Ferreira <sergiolcf.dev@hotmail.com>
2021-11-17 20:12:12 -0300

    Ajustes ErroView Model

--- src/DevIO.Business/Services/FornecedorService.cs
+++ src/DevIO.Business/Services/FornecedorService.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Threading.Tasks;
 using DevIO.Business.Intefaces;
+using DevIO.Business.Interfaces;
 using DevIO.Business.Models;
 using DevIO.Business.Models.Validations;
 
